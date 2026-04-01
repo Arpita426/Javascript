19ee70309a25b26e7ee98c22f1ab685ae35f31f0
Arpita6379 <23ce143@charusat.edu.in>
2026-04-01 09:22:43 +0530

    lecture 1 and lecture 2

---
+++ 01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 12356
+let accountEmail = "Rk8yI@example.com"
+var accountPassword = "123456"
+accountCity = "Mumbai"
+let accountState;
+
+accountEmail = 90
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var
+*/ 
---
+++ 02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict" //strict mode __ everything will be treated as newer version of js 
+
+//alert(5+6)        //we are using nodejs not browser   
+
+let result = console.log(7+9);
+console.log(result)
+console.log("Arpita")
+
+console.log(console.log("Arpita"))
+
+//ECMA  && MDN
+
+//2 power 53
+//primitive datatype
+//string
+//bigint
+//number
+//boolean
+//null --> is a datatype & stanalone value
+//undefined
+//symbol --> unique 
+
+
+console.log(typeof null)  //object
+console.log(typeof undefined)       //undefined
