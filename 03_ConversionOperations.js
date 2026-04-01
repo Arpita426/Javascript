02e78b5eca775beedae5289e1f40a25f521e6bc8
Arpita426 <arpitavekariya6379@gmail.com>
2026-04-01 10:14:32 +0530

    Conversions of Datatypes

---
+++ 03_ConversionOperations.js
@@ -0,0 +1,28 @@
+//let score = "100sdv"
+//let score = null
+let score = undefined
+
+console.log(score)
+console.log(typeof Number(score))
+console.log(Number(score))
+
+/*
+    "sdf" -> NaN
+    null -> 0
+    undefined -> NaN
+    "1234" -> 1234
+    false -> 0
+    true -> 1
+*/ 
+
+let boolean = 0
+console.log(typeof boolean)
+console.log(Boolean(boolean))
+
+/*
+    0 -> false
+    1 -> true
+    "" -> false
+    "arpita" -> true
+*/
+
